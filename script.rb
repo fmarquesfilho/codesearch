dfb866f9f3f4a21d9f5312f2519899468c202e81
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2013-08-02 20:49:01 -0300

    Adicionando OAuth

--- script.rb
+++ script.rb
@@ -1,5 +1,8 @@
 # Usage: $ ruby script.rb [repositories|code|issues|users] [query_string] [OPTIONAL stars|forks|updated] [OPTINAL asc|desc]
 require 'httparty'
+require 'launchy'
+require 'socket'
+require 'awesome_print'
 
 params = {}
 params[:type]  = ARGV[0]
@@ -77,6 +80,41 @@ if params[:type].nil? || params[:q].nil? || !%w[repositories code issues users].
   exit
 end
 
+#START OAuth
+
+	client_id = 'd74fcf012a3e382c7fed'
+	client_secret = '653999ffa39d81e9ae4b0138d4685045a625d967' 
+
+	#open authorize page
+	Launchy.open ('https://github.com/login/oauth/authorize?client_id=' + client_id)
+
+	input = nil
+
+	#start server to get value of code
+	puts 'Starting up server...'
+
+	server = TCPServer.new(2000)
+	loop do
+  	t = Thread.new(server.accept) do |session|
+    	puts "[log] Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
+   		puts "[log] Got input from client"
+    	input = session.gets
+    	session.puts "Permissions granted!\nClosing connection.\nBye :)"
+    	puts "[log] Closing connection"
+    	session.close
+  	end
+  	t.join()
+  	break if !t.status
+	end
+
+	code = input.split("?code=")[1].split(" ")[0]
+
+	#get token
+	token = HTTParty.post('https://github.com/login/oauth/access_token', 
+        :query => {:client_id => client_id, :client_secret => client_secret, :code => code})
+
+#END OAuth
+
 params[:q] = params[:q].gsub(/\s/, '+')
 
 url = "https://api.github.com/search/#{params[:type]}?q=#{params[:q]}"
@@ -88,7 +126,7 @@ headers = { 'Accept' => 'application/vnd.github.preview.text-match+json', 'User-
 puts "URL: #{url}"
 response = HTTParty.get(url, :headers => headers)
 
-raise response.inspect
+ap response
 
 unless params[:csv].nil?
 end
